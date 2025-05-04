5f54fee096da97b5feb29d3badb29ff0faf13eba
SPENCER058 <89460792+SPENCER058@users.noreply.github.com>
2025-05-04 12:16:58 +0700

    feat: add onwork

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import Image from "next/image";
+import webLogo from "@/public/assets/images/logo.png";
+
 export default function Home() {
   return (
     <>
-      <div>
-        Page
+      <div className="h-screen flex flex-col justify-center items-center gap-4">
+        <Image src={webLogo} width={75} height={75} alt="website logo" />
+        <h1 className="h-fit text-3xl font-bold text-gray-500">
+          On Development, stay tuned!
+        </h1>
       </div>
     </>
   );

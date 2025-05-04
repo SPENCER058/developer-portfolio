d3f2045bc1bdf8587109234aa4b9117e114d9746
SPENCER058 <89460792+SPENCER058@users.noreply.github.com>
2025-05-04 12:28:41 +0700

    fix: web image corrupt

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
-import webLogo from "@/public/assets/images/logo.png";
 
 export default function Home() {
   return (
     <>
       <div className="h-screen flex flex-col justify-center items-center gap-4">
-        <Image src={webLogo} width={75} height={75} alt="website logo" />
+        <Image src={"/assets/images/logo.png"} width={75} height={75} alt="website logo" />
         <h1 className="h-fit text-3xl font-bold text-gray-500">
           On Development, stay tuned!
         </h1>
